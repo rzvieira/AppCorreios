7276fd01e363137f98c545952a89c98c52d31d66
rzvieira <rz.vieira@outlook.com>
2018-09-16 22:34:30 -0300

    Add project files.

---
+++ AppConsultaCorreios/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AppConsultaCorreios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //var client = new ServiceCorreio.ServiceClient();
+            //var response = client.buscaEventosAsync("mundofast", "220100ra", "L", "T", "101", "LZ783294953CN");
+
+            test();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        static async void test()
+        {
+            WebServiceCorreios.AtendeClienteClient client = new WebServiceCorreios.AtendeClienteClient();
+
+            string[] list = { "LZ783294953CN", "RS221667342DE", "RS221667475DE" };
+            var response = await client.consultaSROAsync(list, "L", "T", "ECT", "SRO");
+        }
+    }
+}
